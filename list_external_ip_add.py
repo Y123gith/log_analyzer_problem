a416540cf762386cdeebf74be3c3bf10ca77c78e
Y123gith <yaakovde123@gmail.com>
2026-02-11 14:02:48 +0200

    Update list_external_ip_add.py
    removed the print line

--- list_external_ip_add.py
+++ list_external_ip_add.py
@@ -5,5 +5,4 @@ def list_external_ips(log_file):
         file = csv.reader(f)
         list_source_ips = [ip[1] for ip in file]
     external_source_ips = [ip for ip in list_source_ips if ip[:3]!="10." and ip[:7]!="192.168"]
-    return external_source_ips  
-print(list_external_ips("text.csv"))
+    return external_source_ips
