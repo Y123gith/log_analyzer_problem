84b77cc971037138ef50a62bfbd62591b39eda36
Y123gith <yaakovde123@gmail.com>
2026-02-11 16:31:56 +0200

    Update ip_source_access_number.py

--- ip_source_access_number.py
+++ ip_source_access_number.py
@@ -6,4 +6,3 @@ def ip_source_access_counter(log_file):
         ip_sources = [ip[1] for ip in file]
         count_access_by_ip = {ip:ip_sources.count(ip) for ip in ip_sources}
     return count_access_by_ip
-print(ip_source_access_counter("text.csv"))
